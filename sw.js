8c595644225aa08af6699a5a5b38b9ccf9736e22
pierrotyuta-true <pierrotyuta@gmail.com>
2026-03-27 06:29:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
-self.addEventListener('install', (p) => {
-  self.skipWaiting();
+// 숫자를 v1에서 v2로 올리는 것만으로도 업데이트를 유도합니다.
+const CACHE_NAME = 'story-master-v2'; 
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // 새로운 서비스 워커가 즉시 활성화되도록 함
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (p) => {
-  p.respondWith(fetch(p.request));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key); // 옛날 캐시 삭제
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
