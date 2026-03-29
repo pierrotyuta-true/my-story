0994601f32e5e102ecb20cf50ebe77f8a0583d43
pierrotyuta-true <pierrotyuta@gmail.com>
2026-03-29 12:09:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yuta-engine-v2.1';
+const CACHE_NAME = 'yuta-engine-v2.2';
 const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
